2b181f18b49a1187d14d8eb5c9f8de5c76406656
Jonn Nilsson <jonn@jonn.nu>
2024-07-28 23:21:56 +0200

    Add 12 idea slots to rebalanced ideas mod

---
+++ rebalanced_ideas/common/defines/rebalanced_ideas.lua
@@ -0,0 +1,4 @@
+NDefines.NCountry.FREE_IDEA_GROUP_COST  = 4 -- every x.th idea, you'll unlock a new national idea
+NDefines.NCountry.IDEA_TO_TECH = -0.015 -- percentage on tech reduction per idea.
+NDefines.NCountry.MAX_ACTIVE_POLICIES = 7 -- how many active policies at once.
+NDefines.NCountry.PS_BUY_IDEA = 250 -- cost of each ideas 
